a9f73598d7fa69fee1cd8e24bc043ddac764b3b8
Ivannikov Vasilii <133503789+Bazil-Ronin@users.noreply.github.com>
2023-10-14 17:04:50 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("Hello Git")
+from datetime import datetime
+data = {}
+with open('/Users/bazil/Desktop/diary.txt', 'r', encoding='utf-8') as input_file, open('/Users/bazil/Desktop/new_diary.txt', 'w', encoding='utf-8') as output_file:
+    for line in input_file:
+        if datetime.strptime(line, '%d.%m.%Y; %H:%M\n'):
+            print(line)
+        else:
+            print('1')
